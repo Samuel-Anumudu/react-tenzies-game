aeffb99553afd3a76d5b71d20718f9e998fa4191
Samuel Anumudu <samuelmaize55@yahoo.com>
2021-12-28 20:15:31 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+const App = () => {
+  return (
+    <main>
+      <div className="box">
+        <div className="box--inner"></div>
+      </div>
+    </main>
+  );
+};
+
+export default App;
