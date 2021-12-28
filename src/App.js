93c8c201d4955a8f3f53d78e8f214e3ea601e82a
Samuel Anumudu <samuelmaize55@yahoo.com>
2021-12-28 22:24:19 +0200

    created Die component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
+import Die from "./component/Die/Die";
 import "./style.css";
 
-const App = () => {
+export default function App() {
   return (
     <main>
-      <div className="box">
-        <div className="box--inner"></div>
+      <div className="dice-container">
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="3" />
+        <Die value="4" />
+        <Die value="5" />
+        <Die value="6" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
       </div>
     </main>
   );
-};
-
-export default App;
+}
